a146459e6db7c006564e901667de431533ea8c19
Davi Alves <davicdsalves@gmail.com>
2016-10-13 16:48:53 -0300

    add gitignore. reformat script. use enviroment key for parsehub. add error handling for wget in case of 404 (are they shaping our traffic?)

--- flight_download_pdfs.py
+++ flight_download_pdfs.py
@@ -1,56 +1,68 @@
 import json
 import os
+from urllib.error import HTTPError
+import time
 import wget
 from datetime import datetime
 from ph2 import ParseHub
-ph = ParseHub('')
+
+ph = ParseHub(os.environ.get('PARSEHUB_KEY'))
+
 
 def getProjectData():
-	currentYear = datetime.now().year
-	projects = ph.projects
-	print(projects)
+    currentYear = datetime.now().year
+    projects = ph.projects
+    print(projects)
 
-	for i in range(0,4):
-		flightYear = (currentYear - i)
-		filename = 'voo_%d.json' % flightYear
-		print(filename)
-		if not os.path.isfile(filename):
-				projectData = projects[i].last_ready_run.get_data()
-				with open(filename, 'w') as file:
-					json.dump(projectData, file)
+    for i in range(0, 4):
+        flightYear = (currentYear - i)
+        filename = 'voo_%d.json' % flightYear
+        print(filename)
+        if not os.path.isfile(filename):
+            projectData = projects[i].last_ready_run.get_data()
+            with open(filename, 'w') as file:
+                json.dump(projectData, file)
 
-		parseProjectData(filename, flightYear)
-	print('done downloading pdfs')
+        parseProjectData(filename, flightYear)
+    print('done downloading pdfs')
 
 
 def parseProjectData(filename, flightYear):
-	with open(filename) as projectData:
-		data = json.load(projectData)
-	createDir(flightYear)
+    with open(filename) as projectData:
+        data = json.load(projectData)
+    createDir(flightYear)
 
-	months = data["month"]
-	for month in months:
-		if 'days' in month:
-			getMonthData(flightYear, month["name"], month["days"])
+    months = data["month"]
+    for month in months:
+        if 'days' in month:
+            getMonthData(flightYear, month["name"], month["days"])
 
 
 def getMonthData(flightYear, monthName, monthDays):
-	monthFolder = '%s/%s' % (flightYear,monthName)
-	createDir(monthFolder)
-	print('\nparsing %s(%s)' % (monthName,flightYear))
-	
-	for day in monthDays:
-		pdfUrl = day["url"]
-		pdfUrlSplit = pdfUrl.split('/')
-		pdfName = pdfUrlSplit[len(pdfUrlSplit)-1]
-		localPdfFile = '%s/%s' % (monthFolder, pdfName)
+    monthFolder = '%s/%s' % (flightYear, monthName)
+    createDir(monthFolder)
+    print('\nparsing %s(%s)' % (monthName, flightYear))
 
-		if not os.path.isfile(localPdfFile):
-			print('\ndownloading %s-%s[%s]' % (monthName, day["name"], pdfUrl))
-			wget.download(pdfUrl, monthFolder)
+    for day in monthDays:
+        pdfUrl = day["url"]
+        pdfUrlSplit = pdfUrl.split('/')
+        pdfName = pdfUrlSplit[len(pdfUrlSplit) - 1]
+        localPdfFile = '%s/%s' % (monthFolder, pdfName)
+
+        if not os.path.isfile(localPdfFile):
+            print('\ndownloading %s-%s[%s]' % (monthName, day["name"], pdfUrl))
+            try:
+                wget.download(pdfUrl, monthFolder)
+            except HTTPError as e:
+                if e.code == 404:
+                    print('\nerror downloading %s, sleep for 3 seconds.' % pdfUrl)
+                    time.sleep(3)
+                    wget.download(pdfUrl, monthFolder)
+
 
 def createDir(name):
-	if not os.path.exists(str(name)):
-		os.makedirs(str(name))
+    if not os.path.exists(str(name)):
+        os.makedirs(str(name))
+
 
 getProjectData()
