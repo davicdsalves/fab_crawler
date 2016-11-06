35389a28b7fcd08cb0a078caa94e7bd9397df2b9
Davi Alves <davicdsalves@gmail.com>
2016-11-06 17:59:48 -0200

    fix script.

--- db.sql
+++ db.sql
@@ -50,6 +50,5 @@ CREATE TABLE raw_data (
   destino              VARCHAR(255) NOT NULL,
   data_pouso           DATETIME     NOT NULL,
   motivo               VARCHAR(255) NOT NULL,
-  previsao_passageiros INT          NOT NULL,
-  PRIMARY KEY (ID)
+  previsao_passageiros INT          NOT NULL
 );
