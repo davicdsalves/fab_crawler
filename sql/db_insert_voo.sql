82c10cd4ea842c56288726a1d6371d1a9090e9e1
Davi Alves <davicdsalves@gmail.com>
2016-11-13 02:20:48 -0200

    add sql script to insert GABINETE and AUTORIDADE.

--- sql/db_insert_voo.sql
+++ sql/db_insert_voo.sql
@@ -1,3 +1,6 @@
 # adiciona informações na tabela de voo, dados repetidos sao ignorados, tabela tem index unico.
 INSERT IGNORE INTO VOO (ORIGEM, DESTINO, DT_DECOLAGEM, DT_POUSO)
   (select origem, destino, data_decolagem, data_pouso from raw_data);
+
+
+
